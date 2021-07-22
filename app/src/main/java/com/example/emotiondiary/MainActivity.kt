70498a9ab12bc621517f7a8fe60cfc03f26ddcba
yunsung <12345ggh81@gmail.com>
2021-07-23 00:02:40 +0900

    [UPDATE] Animaion_UI Logic modified

--- app/src/main/java/com/example/emotiondiary/MainActivity.kt
+++ app/src/main/java/com/example/emotiondiary/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.emotiondiary
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.emotiondiary.databinding.ActivityMainBinding
@@ -24,57 +25,8 @@ class MainActivity : AppCompatActivity() {
         binding.apply {
             lifecycleOwner = this@MainActivity
             binding.mainActivity = this@MainActivity
+            binding.viewModel = viewmodel
         }
 
-
-        viewmodel.visBoolean.observe(this, Observer { it ->
-            viewmodel.isValue.observe(this, Observer {init ->
-//                if(init == true){
-//                    if(it==true) {
-//                        goneRecyclerView(binding.emotionRecyclerview)
-//                        goneRecyclerView(binding.monthDiaryRecyclerview)
-//                        isTextView(binding.enrollmentText)
-//                        isTextView(binding.enrollmentText2)
-//                    }
-//
-//                    else if(it==false){
-//                        igoneTextView(binding.enrollmentText)
-//                        igoneTextView(binding.enrollmentText2)
-//                        isRecyclerView(binding.monthDiaryRecyclerview)
-//                        isRecyclerView(binding.emotionRecyclerview)
-//                    }
-//                }
-//
-//                else if(init == false){
-//                    if(it==true) {
-//                        goneRecyclerView(binding.monthDiaryRecyclerview)
-//                        goneRecyclerView(binding.emotionRecyclerview)
-//
-//                        isImageView(binding.emotionImage)
-//                        isTextView(binding.emotionText)
-//
-//                    }
-//
-//                    else if(it==false){
-//                        igoneTextView(binding.enrollmentText)
-//                        igoneTextView(binding.enrollmentText2)
-//                        isRecyclerView(binding.monthDiaryRecyclerview)
-//                        isRecyclerView(binding.emotionRecyclerview)
-//                    }
-//                }
-            })
-        })
-
-    }
-
-
-    fun todayBtnClick() {
-        val viewmodel = ViewModelProvider(this).get(Viewmodel::class.java)
-        viewmodel.visBoolean.value = true
-//        startActivity(Intent(this,WriteActivity::class.java))
-    }
-    fun monthBtnClick(){
-        val viewmodel = ViewModelProvider(this).get(Viewmodel::class.java)
-        viewmodel.visBoolean.value = false
     }
 }
--- app/src/main/java/com/example/emotiondiary/viewmodel/Viewmodel.kt
+++ app/src/main/java/com/example/emotiondiary/viewmodel/Viewmodel.kt
@@ -6,31 +6,40 @@ import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.*
 import com.example.emotiondiary.R
 import com.example.emotiondiary.databinding.ActivityMainBinding
+import com.example.emotiondiary.databinding.ActivityMainBindingImpl
 import com.example.emotiondiary.room.Entitys
 import com.example.emotiondiary.room.RoomAppDB
 
 class Viewmodel(application: Application) : AndroidViewModel(application) {
 
     private var allEmotion : MutableLiveData<List<Entitys>>
+    private lateinit var binding: ActivityMainBinding
 
-    //Room에 값이 들어가 있는지 아닌지 판단
-    private val _isValue = MutableLiveData<Boolean>()
-
-    val isValue : LiveData<Boolean>
-        get() = _isValue
-
-    //가시성 판단
+    //UI 가시성
     private val _visBoolean = MutableLiveData<Boolean>()
     val visBoolean: MutableLiveData<Boolean>
         get() = _visBoolean
 
+    //Room에 값이 들어가 있는지 아닌지 판단
+    private val _isValue = MutableLiveData<Boolean>()
+    val isValue : MutableLiveData<Boolean>
+        get() = _isValue
+
     init {
         allEmotion = MutableLiveData()
+        _isValue.value = true
     }
 
     fun isValue(data : Entitys){
         if(data.emotionDiary.isEmpty()){
         }
+    }
+    fun todayBtnClick() {
+            _visBoolean.value = true
+//        startActivity(Intent(this,WriteActivity::class.java))
+    }
+    fun monthBtnClick(){
+            _visBoolean.value = false
     }
 
     private fun getTodayEmotion(){
