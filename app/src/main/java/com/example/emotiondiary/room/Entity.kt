aec091396bc6169e4bcb361fe6bc93a7846b0f74
yunsung <12345ggh81@gmail.com>
2021-08-09 21:08:45 +0900

    [UPDATE] entity 구성요소 수정

--- app/src/main/java/com/example/emotiondiary/room/Entity.kt
+++ app/src/main/java/com/example/emotiondiary/room/Entity.kt
@@ -6,11 +6,20 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "emotion")
 data class Entitys(
+    //감정일기 타이틀
     @ColumnInfo(name = "title") val title : String,
+    //감정일기 내용
     @ColumnInfo(name = "contents") val contents : String,
-    @ColumnInfo(name = "emotionDiary") val emotionDiary : String,
-    @ColumnInfo(name = "color") val color : String
-){
+    //감정일기 날짜
+    @ColumnInfo(name = "Date") val emotionDiary : String,
+    //감정 색
+    @ColumnInfo(name = "color") val color : String,
+    //감정
+    @ColumnInfo(name = "emotion") val emotion : String,
+    //유저가 글을 쓴 날짜
+    @ColumnInfo(name = "date") val date : String
+    ){
+    //유저 아이디
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "id") var id : Int = 0
 
