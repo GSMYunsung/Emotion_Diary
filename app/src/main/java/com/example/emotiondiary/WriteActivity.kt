372569595c62755298ec9180443095fa5e5be792
yunsung <12345ggh81@gmail.com>
2021-08-13 12:17:01 +0900

    [UPDATE] error Logic modified , emotion change Logic add

--- app/src/main/java/com/example/emotiondiary/WriteActivity.kt
+++ app/src/main/java/com/example/emotiondiary/WriteActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
@@ -25,7 +26,7 @@ class WriteActivity : AppCompatActivity() {
 
     lateinit var navController : NavController
     private val binding by lazy { ActivityWriteBinding.inflate(layoutInflater) }
-    private val bindingwrite by lazy { FragmentWriteBinding.inflate(layoutInflater) }
+    private val bindingWrite by lazy { FragmentWriteBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,9 +39,39 @@ class WriteActivity : AppCompatActivity() {
     }
 
     fun changeBackground(v : View){
+
         when(v.id){
-            R.id.choose_angry_emotion_image ->{
-                bindingwrite.chooseAngryEmotionImage.setImageResource(R.drawable.angry)
+            R.id.choose_angry_emotion_image -> {
+                findViewById<ImageView>(R.id.emotion_image).setImageResource(R.drawable.angry)
+                findViewById<TextView>(R.id.emotion_text).text = getString(R.string.emotion_angry)
+            }
+            R.id.choose_cute_emotion_image ->{
+             findViewById<ImageView>(R.id.emotion_image).setImageResource(R.drawable.cute)
+             findViewById<TextView>(R.id.emotion_text).text = getString(R.string.emotion_cute)
+            }
+            R.id.choose_sad_emotion_image ->{
+                findViewById<ImageView>(R.id.emotion_image).setImageResource(R.drawable.sad)
+                findViewById<TextView>(R.id.emotion_text).text = getString(R.string.emotion_sad)
+            }
+            R.id.choose_dizzy_emotion_image ->{
+                findViewById<ImageView>(R.id.emotion_image).setImageResource(R.drawable.dizzy)
+                findViewById<TextView>(R.id.emotion_text).text = getString(R.string.emotion_dizzy)
+            }
+            R.id.choose_fun_emotion_image ->{
+                findViewById<ImageView>(R.id.emotion_image).setImageResource(R.drawable.`fun`)
+                findViewById<TextView>(R.id.emotion_text).text = getString(R.string.emotion_happy)
+            }
+            R.id.choose_looksly_emotion_image ->{
+                findViewById<ImageView>(R.id.emotion_image).setImageResource(R.drawable.looksly)
+                findViewById<TextView>(R.id.emotion_text).text = getString(R.string.emotion_looksly)
+            }
+            R.id.choose_whistle_emotion_image ->{
+                findViewById<ImageView>(R.id.emotion_image).setImageResource(R.drawable.whistle)
+                findViewById<TextView>(R.id.emotion_text).text = getString(R.string.emotion_whistle)
+            }
+            R.id.choose_sleep_emotion_image ->{
+                findViewById<ImageView>(R.id.emotion_image).setImageResource(R.drawable.sleep)
+                findViewById<TextView>(R.id.emotion_text).text = getString(R.string.emotion_sleep)
             }
         }
     }
