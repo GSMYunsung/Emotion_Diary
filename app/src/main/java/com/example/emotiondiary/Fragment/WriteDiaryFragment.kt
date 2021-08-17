e246d73e95ab9ded5c5f9eb428b6ae08f64ce9a2
yunsung <12345ggh81@gmail.com>
2021-08-17 21:29:46 +0900

    [UPDATE] navigation_ color send Logic add

--- app/src/main/java/com/example/emotiondiary/Fragment/WriteDiaryFragment.kt
+++ app/src/main/java/com/example/emotiondiary/Fragment/WriteDiaryFragment.kt
@@ -9,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
@@ -25,9 +26,7 @@ class WriteDiaryFragment : Fragment() {
     ): View? {
         val view: View = inflater!!.inflate(R.layout.fragment_write_diary, container, false)
 
-        view.findViewById<ImageView>(R.id.emotion_image).backgroundTintList =
-            ColorStateList.valueOf(
-                ContextCompat.getColor(requireContext(), safeArgs.color.toInt()))
+        setColor(view)
 
         view.findViewById<Button>(R.id.next_btn2).setOnClickListener {
             getActivity()?.finish()
@@ -42,5 +41,102 @@ class WriteDiaryFragment : Fragment() {
         }
 
         return view
+    }
+
+    private fun setColor(v : View) {
+        when (safeArgs.color) {
+        "white" -> {
+            v.findViewById<ImageView>(R.id.emotion_image)
+            .setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.white))
+        }
+        "teal" ->{
+            v.findViewById<ImageView>(R.id.emotion_image)
+                .setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.teal_200))
+        }
+        "light_green" ->{
+            v.findViewById<ImageView>(R.id.emotion_image)
+                .setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.light_green))
+        }
+        "dark_gray" ->{
+            v.findViewById<ImageView>(R.id.emotion_image)
+                .setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.dark_gray))
+        }
+        "pink" -> {
+            v.findViewById<ImageView>(R.id.emotion_image)
+                .setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.pink))
+        }
+        "orange" ->{
+            v.findViewById<ImageView>(R.id.emotion_image)
+                .setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.orange))
+        }
+        "ivory" ->{
+            v.findViewById<ImageView>(R.id.emotion_image)
+                .setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.ivory))
+        }
+        "lemon" ->{
+            v.findViewById<ImageView>(R.id.emotion_image)
+                .setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.lemon))
+        }
+        "grapefruit" ->{
+            v.findViewById<ImageView>(R.id.emotion_image)
+                .setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.grapefruit))
+        }
+        "green" ->{
+            v.findViewById<ImageView>(R.id.emotion_image)
+                .setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.green))
+        }
+        "purple" ->{
+            v.findViewById<ImageView>(R.id.emotion_image)
+                .setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.purple_200))
+        }
+        "gray" ->{
+            v.findViewById<ImageView>(R.id.emotion_image)
+                .setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.gray))
+        }
+    }
+    }
+    private fun setBackground(v : View){
+
+        when(v.id){
+            R.id.choose_angry_emotion_image -> {
+
+                v.findViewById<ImageView>(R.id.emotion_image).setImageResource(R.drawable.angry)
+                v.findViewById<TextView>(R.id.emotion_text).text = getString(R.string.emotion_angry)
+            }
+            R.id.choose_cute_emotion_image ->{
+
+                v.findViewById<ImageView>(R.id.emotion_image).setImageResource(R.drawable.cute)
+                v.findViewById<TextView>(R.id.emotion_text).text = getString(R.string.emotion_cute)
+            }
+            R.id.choose_sad_emotion_image ->{
+
+                v.findViewById<ImageView>(R.id.emotion_image).setImageResource(R.drawable.sad)
+                v.findViewById<TextView>(R.id.emotion_text).text = getString(R.string.emotion_sad)
+            }
+            R.id.choose_dizzy_emotion_image ->{
+
+                v.findViewById<ImageView>(R.id.emotion_image).setImageResource(R.drawable.dizzy)
+                v.findViewById<TextView>(R.id.emotion_text).text = getString(R.string.emotion_dizzy)
+            }
+            R.id.choose_fun_emotion_image ->{
+
+                v.findViewById<ImageView>(R.id.emotion_image).setImageResource(R.drawable.`fun`)
+                v.findViewById<TextView>(R.id.emotion_text).text = getString(R.string.emotion_happy)
+            }
+            R.id.choose_looksly_emotion_image ->{
+
+                v.findViewById<ImageView>(R.id.emotion_image1).setImageResource(R.drawable.looksly)
+                v.findViewById<TextView>(R.id.emotion_text).text = getString(R.string.emotion_looksly)
+            }
+            R.id.choose_whistle_emotion_image ->{
+
+                v.findViewById<ImageView>(R.id.emotion_image1).setImageResource(R.drawable.whistle)
+                v.findViewById<TextView>(R.id.emotion_text).text = getString(R.string.emotion_whistle)
+            }
+            R.id.choose_sleep_emotion_image ->{
+                v.findViewById<ImageView>(R.id.emotion_image1).setImageResource(R.drawable.sleep)
+                v.findViewById<TextView>(R.id.emotion_text).text = getString(R.string.emotion_sleep)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/emotiondiary/Fragment/WriteFragment.kt
+++ app/src/main/java/com/example/emotiondiary/Fragment/WriteFragment.kt
@@ -1,5 +1,12 @@
 package com.example.emotiondiary.Fragment
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.res.Resources
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -8,7 +15,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.Navigation.findNavController
@@ -18,10 +28,12 @@ import androidx.navigation.fragment.navArgs
 import com.example.emotiondiary.R
 import com.example.emotiondiary.databinding.ActivityMainBinding
 import com.example.emotiondiary.databinding.FragmentWriteBinding
+import com.example.emotiondiary.viewmodel.Viewmodel
 
 class WriteFragment : Fragment() {
 
     lateinit var navController : NavController
+    private val binding2 by lazy { FragmentWriteBinding.inflate(layoutInflater) }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -31,7 +43,7 @@ class WriteFragment : Fragment() {
         view.findViewById<Button>(R.id.next_btn).setOnClickListener {
 
             view.findNavController().navigate( WriteFragmentDirections
-                .actionEmotionFragmentToDiaryFragment(color = view.findViewById<ImageView>(R.id.emotion_image).backgroundTintList.toString()))
+                .actionEmotionFragmentToDiaryFragment(color = colorCase(),emotion = emotionCase()))
         }
 
         view.findViewById<Button>(R.id.finish_btn).setOnClickListener {
@@ -46,5 +58,28 @@ class WriteFragment : Fragment() {
 
         navController = findNavController(view)
 
+    }
+
+    private fun emotionCase(): String {
+        return "내일해야함"
+    }
+
+    fun colorCase() : String{
+       when((view?.findViewById<ImageView>(R.id.emotion_image1)?.background as ColorDrawable).color)
+       {
+           ContextCompat.getColor(requireContext(),R.color.white) -> return "white"
+           ContextCompat.getColor(requireContext(),R.color.teal_200) -> return "teal"
+           ContextCompat.getColor(requireContext(),R.color.light_green) -> return "light_green"
+           ContextCompat.getColor(requireContext(),R.color.dark_gray) -> return "dark_gray"
+           ContextCompat.getColor(requireContext(),R.color.pink) -> return "pink"
+           ContextCompat.getColor(requireContext(),R.color.orange) -> return "orange"
+           ContextCompat.getColor(requireContext(),R.color.ivory) -> return "ivory"
+           ContextCompat.getColor(requireContext(),R.color.lemon) -> return "lemon"
+           ContextCompat.getColor(requireContext(),R.color.grapefruit) -> return "grapefruit"
+           ContextCompat.getColor(requireContext(),R.color.green) -> return "green"
+           ContextCompat.getColor(requireContext(),R.color.purple_200) -> return "purple"
+           ContextCompat.getColor(requireContext(),R.color.gray) -> return "gray"
+       }
+        return "none"
     }
 }
